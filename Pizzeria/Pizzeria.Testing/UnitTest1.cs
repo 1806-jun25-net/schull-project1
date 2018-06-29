04ce16ecf28af966d1c91133020210a1c3927e52
Wesley Schull <wcschull@bsu.edu>
2018-06-29 17:12:52 -0400

    Add test for User.ShowName()

--- Pizzeria/Pizzeria.Testing/UnitTest1.cs
+++ Pizzeria/Pizzeria.Testing/UnitTest1.cs
@@ -6,9 +6,16 @@ namespace Pizzeria.Testing
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void UserShowNameShouldShowFullName(User user)
         {
-
+            // arrange
+            user.FirstName = "Wesley";
+            user.LastName = "Schull";
+            var expected = "Wesley Schll";
+            // act
+            var actual = user.ShowName();
+            // assert
+            Assert.Equal(expected, actual);
         }
     }
 }
