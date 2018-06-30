ca34b04b065f8314e877c11bf2fc2fca1776b9e4
Wesley Schull <wcschull@bsu.edu>
2018-06-30 15:45:19 -0400

    Implemented ILocation and IToppings (realized I might need toppings to be their own class)

---
+++ Pizzeria/Pizzeria.Library/ILocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria.Library
+{
+    public interface ILocation
+    {
+        string LocationName { get; set; }
+        List<IToppings> Inventory { get; set; }
+    }
+    bool CheckOrderValidity(IOrder order)
+    {
+
+    }
+}
---
+++ Pizzeria/Pizzeria.Library/IToppings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria.Library
+{
+    public interface IToppings
+    {
+        string Topping { get; set; }
+        int InventoryCount { get; set; }
+    }
+}
