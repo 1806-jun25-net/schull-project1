9d13edc5ecf529c75ef6cea0cb07ff0230eb25cc
Wesley Schull <wcschull@bsu.edu>
2018-06-30 15:33:53 -0400

    Created iPizza interface

--- Pizzeria/Pizzeria.Library/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Pizzeria.Library
-{
-    public class Class1
-    {
-    }
-}
---
+++ Pizzeria/Pizzeria.Library/IPizza.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria.Library
+{
+    public interface IPizza
+    {
+        string[] Toppings { get; set; }
+        double Price { get; set; }
+    }
+}
