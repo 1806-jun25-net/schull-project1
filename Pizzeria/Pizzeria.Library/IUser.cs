bf6d9a630cb5ed8265906424d892cc6d603e8786
Wesley Schull <wcschull@bsu.edu>
2018-06-30 15:36:58 -0400

    Implemented IUser interface (Clarification: NOT THE UI)

---
+++ Pizzeria/Pizzeria.Library/IUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzeria.Library
+{
+    public interface IUser
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string DefaultLocation { get; set; }
+        DateTime LastOrder { get; set; }
+    }
+}
