b31f64c9ffd053fd43a9062ba6de79f33c8972f3
Wesley Schull <wcschull@bsu.edu>
2018-07-04 13:15:18 -0400

    Fixed some issues with one of the commands

--- Pizzeria/Pizzeria.UI/Program.cs
+++ Pizzeria/Pizzeria.UI/Program.cs
@@ -18,15 +18,21 @@ namespace Pizzeria.UI
             Order order = new Order();
             Pizza pizza = new Pizza();
             Ingredient ingredient = new Ingredient("", 0);
-
-            Console.WriteLine("Pizzeria App: Console ver 1.0\n" +
-                "Your input for this application will be based on numeric values.\n\n");
             var input = 1;
 
+            Console.WriteLine("Pizzeria App: Console ver 1.0\n" +
+                "Your input for this application will be based on numeric values.\n");
+            
             while (input != 0)
             {
                 ShowCommandReference();
-                input = int.Parse(Console.ReadLine());
+                string temp = Console.ReadLine();
+                while(temp == null || temp == "")
+                {
+                    Console.WriteLine("You didn't input anything! Please try again.\n");
+                    temp = Console.ReadLine();
+                }
+                input = int.Parse(temp);
                 switch (input)
                 {
                     case 0: // Exit Application
@@ -43,20 +49,21 @@ namespace Pizzeria.UI
                         Console.WriteLine("Where would you prefer to order from?");
                         newUser.DefaultLocation = Console.ReadLine();
                         Console.WriteLine($"Here's what we know currently: Your name is {newUser.FirstName} {newUser.LastName}," +
-                            $"and you will be ordering from {newUser.DefaultLocation}. You may change this location any time!");
+                            $"and you will be ordering from {newUser.DefaultLocation}. You may change this location any time!\n");
                         break;
                     case 3: // Create New Order
                         Console.WriteLine("Before we proceed, you cannot have more than one active order at any " +
                             "one given time. If you use this command after already creating an order, that " +
                             "previous order will be overwritten.\n" +
-                            "Would you like to proceed with this command? [ Y / N ]");
-                        if(Console.ReadLine() == "Y") // Proceed to Create New Order / Overwrite Previous Order
+                            "Would you like to proceed with this command? [ Y / N ]\n");
+                        string orderDecision = Console.ReadLine().ToLower();
+                        if(orderDecision == "y") // Proceed to Create New Order / Overwrite Previous Order
                         {
                             order = new Order();
                             Console.WriteLine("You have begun your new order, but it's currently empty. " +
-                                "You can use Command 4 to begin creating a new pizza!");
+                                "You can use Command 4 to begin creating a new pizza!\n");
                         }
-                        else if(Console.ReadLine() == "N") // Cancel New Order Creation Process
+                        else if(orderDecision == "n") // Cancel New Order Creation Process
                         {
                             break;
                         }
@@ -67,17 +74,18 @@ namespace Pizzeria.UI
                         }
                         break;
                     case 4: // Create Your Pizza
-                        Console.WriteLine("Before we proceed, you cannot have more than one active order at any " +
-                            "one given time. If you use this command after already creating an order, that " +
-                            "previous order will be overwritten.\n" +
-                            "Would you like to proceed with this command? [ Y / N ]");
-                        if (Console.ReadLine() == "Y") // Proceed to Create New Order / Overwrite Previous Order
+                        Console.WriteLine("Before we proceed, you cannot have more than one active pizza at any " +
+                            "one given time. If you use this command after already creating a pizza, that " +
+                            "previous pizza will be overwritten.\n" +
+                            "Would you like to proceed with this command? [ Y / N ]\n");
+                        string pizzaDecision = Console.ReadLine().ToLower();
+                        if (pizzaDecision == "y") // Proceed to Create New Order / Overwrite Previous Order
                         {
-                            order = new Order();
-                            Console.WriteLine("You have begun your new order, but it's currently empty. " +
-                                "You can use Command 4 to begin creating a new pizza!");
+                            pizza = new Pizza();
+                            Console.WriteLine("You have begun creating your new pizza, but there's nothing on it. " +
+                                "Once you've added your toppings, you can use Command 5 to save it to your order!\n");
                         }
-                        else if (Console.ReadLine() == "N") // Cancel New Order Creation Process
+                        else if (pizzaDecision == "n") // Cancel New Order Creation Process
                         {
                             break;
                         }
@@ -93,6 +101,7 @@ namespace Pizzeria.UI
                             "1 - Pepperoni\n" +
                             "2 - Sausage\n" +
                             "3 - Ham\n");
+                        input = int.Parse(Console.ReadLine());
                         switch(input){
                             case 0: // Done adding toppings; break from method
                                 break;
@@ -102,12 +111,12 @@ namespace Pizzeria.UI
                                 topping.Count = 1;
                                 if (pizza.Toppings.Contains(topping))
                                 {
-                                    Console.WriteLine("This pizza already has pepperoni on it!");
+                                    Console.WriteLine("This pizza already has pepperoni on it!\n");
                                 }
                                 else
                                 {
                                     pizza.AddTopping(topping);
-                                    Console.WriteLine($"This is your pizza currently: {pizza.ToString()}");
+                                    Console.WriteLine($"This is your pizza currently: {pizza.ToString()}\n");
                                 }
                                 break;
                             case 2:
@@ -116,12 +125,12 @@ namespace Pizzeria.UI
                                 topping.Count = 1;
                                 if (pizza.Toppings.Contains(topping))
                                 {
-                                    Console.WriteLine("This pizza already has sausage on it!");
+                                    Console.WriteLine("This pizza already has sausage on it!\n");
                                 }
                                 else
                                 {
                                     pizza.AddTopping(topping);
-                                    Console.WriteLine($"This is your pizza currently: {pizza.ToString()}");
+                                    Console.WriteLine($"This is your pizza currently: {pizza.ToString()}\n");
                                 }
                                 break;
                             case 3:
@@ -130,54 +139,80 @@ namespace Pizzeria.UI
                                 topping.Count = 1;
                                 if (pizza.Toppings.Contains(topping))
                                 {
-                                    Console.WriteLine("This pizza already has ham on it!");
+                                    Console.WriteLine("This pizza already has ham on it!\n");
                                 }
                                 else
                                 {
                                     pizza.AddTopping(topping);
-                                    Console.WriteLine($"This is your pizza currently: {pizza.ToString()}");
+                                    Console.WriteLine($"This is your pizza currently: {pizza.ToString()}\n");
                                 }
                                 break;
                             default:
-                                Console.WriteLine("Incorrect input: aborting operation and returning to safe zone.");
+                                Console.WriteLine("Incorrect input: aborting operation and returning to safe zone.\n");
                                 break;
                         }
-                        order.AddPizza(pizza);
+                        //order.AddPizza(pizza);
                         break;
-                    case 5: 
+                    case 5: // Add Pizza to Order
                         break;
                     case 6:
-                        //
+                        // Add Topping to Pizza
                         break;
                     case 7:
-                        //
+                        // Remove Topping from Pizza (Requires Confirmation)
                         break;
                     case 8:
-                        //
+                        // Remove Pizza from Order (Requires Confirmation)
                         break;
                     case 9:
-                        //
+                        // Show Order Information
                         break;
                     case 10:
-                        //
+                        // Show Pizza Information
                         break;
                     case 11:
-                        //
+                        // Show Location Inventory
                         break;
                     case 12:
-                        //
+                        // Add an Item to Location Inventory
                         break;
                     case 13:
-                        //
+                        // Remove an Item from Location Inventory
                         break;
                     case 14:
-                        //
+                        // Delete Location Inventory (Requires Confirmation)
                         break;
                     case 15:
-                        //
+                        // Change User Default Location
                         break;
                     case 16:
-                        //
+                        // Delete Current Order
+                        if(order.Pizzas == null)
+                        {
+                            Console.WriteLine("There is no order currently.");
+                        }
+                        else if(order.Pizzas.Count == 0)
+                        {
+                            Console.WriteLine("This order is already blank; cannot delete nothing.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Are you sure? [ Y / N ]\n");
+                            string deleteDecision = Console.ReadLine().ToLower();
+                            if (deleteDecision == "y")
+                            {
+                                order.DeleteOrder();
+                                Console.WriteLine("Your order has been erased.");
+                            }
+                            else if (deleteDecision == "n")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Incorrect input: aborting operation and returning to safe zone.\n");
+                            }
+                        }
                         break;
                     default:
                         Console.WriteLine("ERROR: Invalid input.");
@@ -207,7 +242,5 @@ namespace Pizzeria.UI
                 "15 - Change User Default Location\n" +
                 "16 - Delete Current Order\n");
         }
-
-        
     }
 }
